c64fcac4996c0956d244b986886c819a2e811236
Maksym Semenykhin <MaksymSemenykhin@users.noreply.github.com>
2023-09-15 14:41:06 +0300

    Update check_brackets.php

--- check_brackets.php
+++ check_brackets.php
@@ -6,38 +6,40 @@
  * @param string $expression The arithmetic expression to check.
  * @return string Returns "Correct" if brackets are balanced, otherwise "Incorrect".
  */
-function check_brackets($expression) {
-    $stack = [];  // This stack is used to keep track of the opening brackets.
 
+function check_brackets($expression) {
+	
     // Pairs of brackets for easy checking.
-    const BRACKET_PAIRS = [
+    $bracketPairs = [
         ')' => '(',
         ']' => '[',
         '}' => '{'
-    ];
+    ]; 
+
+    $stack = [];  // This stack is used to keep track of the opening brackets.
 
     // Constants for return values.
-    const CORRECT = "Correct";
-    const INCORRECT = "Incorrect";
+    $correct = "Correct";
+    $incorrect = "Incorrect";
 
     // Loop through each character in the expression.
     for ($i = 0; $i < strlen($expression); $i++) {
         $char = $expression[$i];
 
         // If character is an opening bracket, push it onto the stack.
-        if (in_array($char, BRACKET_PAIRS)) {
+        if (in_array($char, $bracketPairs)) {
             array_push($stack, $char);
         } 
         // If character is a closing bracket, check for its corresponding opening bracket in the stack.
-        elseif (array_key_exists($char, BRACKET_PAIRS)) {
+        elseif (array_key_exists($char, $bracketPairs)) {
             // If the stack is empty or doesn't have the matching opening bracket, return "Incorrect".
-            if (count($stack) == 0 || BRACKET_PAIRS[$char] !== array_pop($stack)) {
-                return INCORRECT;
+            if (count($stack) == 0 || $bracketPairs[$char] !== array_pop($stack)) {
+                return $incorrect;
             }
         }
     }
 
     // If the stack is empty after processing the entire string, the brackets are balanced.
-    return count($stack) == 0 ? CORRECT : INCORRECT;
+    return count($stack) == 0 ? $correct : $incorrect;
 }
 ?>
